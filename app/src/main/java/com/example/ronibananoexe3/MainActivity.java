939a17b413dc309363461696a0c2b6671cc052ff
מיטל <ronni.ban@sisma.org.il>
2024-01-02 15:29:13 +0200

    My commit

--- app/src/main/java/com/example/ronibananoexe3/MainActivity.java
+++ app/src/main/java/com/example/ronibananoexe3/MainActivity.java
@@ -10,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     private Button bt;
     int count=0;
 
+    //bbjnl
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
